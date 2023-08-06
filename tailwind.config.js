e9fbbcf29ddddf258bb52f35d7196d85761de557
Neluttu <neluttu@gmail.com>
2023-08-06 18:33:16 +0300

    Need mobile menu

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html','styles.css'],
+  theme: {
+    extend: {
+        fontFamily: {
+            'nunito': ['Nunito', 'sans-serif'],
+            'rubik': ['Rubik', 'sans-serif'],
+            'poppins': ['Poppins', 'sans-serif'],
+            'merriweather': ['Merriweather', 'serif'],
+            'merriweatherSans': ['Merriweather Sans', 'sans-serif'],
+        },
+    },
+  },
+  plugins: [],
+}
